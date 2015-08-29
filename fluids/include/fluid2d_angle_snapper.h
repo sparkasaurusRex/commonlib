a87272dda643147c3d1edeb4685369b254491f98
Chad Foxglove <chandra.foxglove@gmail.com>
2015-08-28 21:03:56 -0700

    angle snapper

---
+++ fluids/include/fluid2d_angle_snapper.h
@@ -0,0 +1,26 @@
+#ifndef __FLUID2D_ANGLE_SNAPPER__
+#define __FLUID2D_ANGLE_SNAPPER__
+
+#include "fluid2d_interactor.h"
+
+class Fluid2DAngleSnapper : public Fluid2DInteractor
+{
+  public:
+    Fluid2DAngleSnapper();
+    Fluid2DAngleSnapper(const int _num_angles);
+    ~Fluid2DAngleSnapper();
+
+    void set_strength(const float s) { strength = s; }
+
+    void simulate(const float dt);
+
+  protected:
+    void allocate_angles();
+
+    int num_angles;
+    Float2 *angles;
+
+    float strength;
+};
+
+#endif
---
+++ fluids/src/fluid2d_angle_snapper.cpp
@@ -0,0 +1,68 @@
+#include <assert.h>
+#include "fluid2d_angle_snapper.h"
+#include "matrix.h"
+
+Fluid2DAngleSnapper::Fluid2DAngleSnapper()
+{
+  num_angles = 6;
+  allocate_angles();
+  strength = 1.0f;
+}
+
+Fluid2DAngleSnapper::Fluid2DAngleSnapper(const int _num_angles)
+{
+  num_angles = _num_angles;
+  strength = 1.0f;
+  allocate_angles();
+}
+
+Fluid2DAngleSnapper::~Fluid2DAngleSnapper()
+{
+  delete angles;
+}
+
+void Fluid2DAngleSnapper::allocate_angles()
+{
+  angles = new Float2[num_angles];
+  Float2 v(1.0f, 0.0f);
+  float rot_angle = 0.0f;
+  for(int i = 0; i < num_angles; i++)
+  {
+    Matrix2x2 m;
+    m.rotation(rot_angle);
+
+    angles[i] = m * v;
+
+    rot_angle += (2.0f * M_PI) / (float)num_angles;
+  }
+}
+
+void Fluid2DAngleSnapper::simulate(const float dt)
+{
+  for(int i = 0; i < fluid_dim[0]; i++)
+  {
+    for(int j = 0; j < fluid_dim[1]; j++)
+    {
+      Float2 vel(u[idx(i, j)], v[idx(i, j)]);
+      float mag = vel.magnitude();
+
+      float max_dp = -1.0f;
+      int closest_angle = -1;
+      for(int a = 0; a < num_angles; a++)
+      {
+        float dp = angles[a] * vel;
+        if(dp > max_dp || closest_angle < 0)
+        {
+          closest_angle = a;
+          max_dp = dp;
+        }
+      }
+
+      assert(closest_angle >= 0);
+      Float2 snap_vel = angles[closest_angle] * mag;
+      Float2 new_vel = lerp(vel, snap_vel, strength);
+      u[idx(i, j)] = snap_vel[0];
+      v[idx(i, j)] = snap_vel[1];
+    }
+  }
+}
