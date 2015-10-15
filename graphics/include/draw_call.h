4857995289836c332a483274357de5e3ae14601c
Chad Foxglove <chandra.foxglove@gmail.com>
2015-10-15 11:54:32 +0400

    draw calls

--- graphics/include/draw_call.h
+++ graphics/include/draw_call.h
@@ -1,16 +1,25 @@
 #ifndef __DRAW_CALL_H__
 #define __DRAW_CALL_H__
 
+
+#if defined(__APPLE__)
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+
 class DrawCall
 {
 public:
   DrawCall();
-  ~DrawCall();
+  ~DrawCall() {}
+
+  void set_num_indices(const int n) { num_indices = n; }
+
 private:
   int num_indices;
 
   GLuint vbo;
-  GLuint nbo;
   GLuint ibo;
 };
 
---
+++ graphics/src/draw_call.cpp
@@ -0,0 +1,8 @@
+#include "draw_call.h"
+
+DrawCall::DrawCall()
+{
+  num_indices = 0;
+  vbo = -1;
+  ibo = -1;
+}
