78efec6749d8793fa021b2090e52b2907d59d221
Chad Foxglove <chandra.foxglove@gmail.com>
2016-03-31 19:26:25 -0700

    aurora

--- graphics/src/vfx/ribbon.cpp
+++ graphics/src/vfx/ribbon.cpp
@@ -12,7 +12,7 @@ Ribbon::Ribbon()
   ibo = 0;
 
   num_verts = 0;
-  num_segments = 10;
+  num_segments = 20;
 
   root_scale = 0.7f;
   root_speed = 0.0001f;
@@ -23,12 +23,12 @@ Ribbon::Ribbon()
   profile_noise_scale = 1.2f;
   profile_noise_speed = 0.0003f;
 
-  theta_bounds[0] = 0.0f;
-  theta_bounds[1] = 0.1f;
+  theta_bounds[0] = 0.0f;//M_PI - 0.1f;
+  theta_bounds[1] = 0.5f;//M_PI;
   phi_bounds[0] = 0.0f;
-  phi_bounds[1] = 0.2f;
+  phi_bounds[1] = 0.5f;
 
-  profile_a.bell_curve_cerp(0.4f, 0.6f);
+  profile_a.bell_curve_cerp(0.45f, 0.55f);
 
   mat = NULL;
   tex_a = NULL;
@@ -156,22 +156,24 @@ void Ribbon::render()
 
 void Ribbon::simulate(const float gt, const float dt)
 {
-  float ribbon_length = 0.2f; //in radians?
+  float theta_offset = 0.0f;
+  float phi_offset = M_PI;
+  float ribbon_length = 0.6f; //in radians?
   //TODO: use dynamic texture and vertex-shader displacement
   for(int i = 0; i < num_segments; i++)
   {
     float seg_pct = (float)i / (float)num_segments;
-    float theta = PerlinNoise::scaled_octave_noise_2d(2, 1.0f, root_scale, theta_bounds[0], theta_bounds[1], gt * root_speed, seg_pct * root_scale);
-    float phi = PerlinNoise::scaled_octave_noise_2d(2, 1.0f, root_scale, phi_bounds[0], phi_bounds[1], gt * root_speed + 137.9532f, seg_pct * root_scale);
-    phi = phi + seg_pct * ribbon_length;
+    float theta = theta_offset + PerlinNoise::scaled_octave_noise_2d(2, 1.0f, root_scale, theta_bounds[0], theta_bounds[1], gt * root_speed, seg_pct);
+    float phi = PerlinNoise::scaled_octave_noise_2d(2, 1.0f, root_scale, phi_bounds[0], phi_bounds[1], gt * root_speed + 137.9532f, seg_pct);
+    phi = phi_offset + phi + seg_pct * ribbon_length;
 
     float profile_height = PerlinNoise::scaled_octave_noise_2d(2, 1.0f, profile_noise_scale, profile_noise_bounds[0], profile_noise_bounds[1], gt * profile_noise_speed + 165.132f, seg_pct * profile_noise_scale);
     profile_height *= profile_a.evaluate(seg_pct);
 
-    Float3 root_vert_pos = polar_to_cartesian(theta, phi, 1.0f);
+    Float3 root_vert_pos = polar_to_cartesian(theta, phi, 1.1f);
     Float3 top_vert_pos = root_vert_pos + root_vert_pos * profile_height;
 
-    Float3 n = top_vert_pos - root_vert_pos;
+    Float3 n = root_vert_pos;
     Float3 tmp = n ^ Float3(0.0f, 1.0f, 0.0f);
     n = n ^ tmp;
     n.normalize();
