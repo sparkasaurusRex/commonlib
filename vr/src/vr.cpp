f88b1d7531febfee3964d40a3078df956040df23
Chad Foxglove <chandra.foxglove@gmail.com>
2016-04-25 22:59:37 -0700

    vr connection to oculus cv1

--- vr/src/vr.cpp
+++ vr/src/vr.cpp
@@ -22,8 +22,37 @@ void VRContext::init()
   result = ovr_Create(&session, &luid);
   assert(OVR_SUCCESS(result));
 
-  //poll HMD
-  ovrHmdDesc hmdDesc = ovr_GetHmdDesc(session);
+  //poll HMD device (Head Mounted Display)
+  ovrHmdDesc hmd_desc = ovr_GetHmdDesc(session);
+
+  // Setup Window and Graphics
+  // Note: the mirror window can be any size, for this sample we use 1/2 the HMD resolution
+  ovrSizei window_size = { hmd_desc.Resolution.w / 2, hmd_desc.Resolution.h / 2 };
+
+  // Make eye render buffers
+ for(int eye = 0; eye < 2; ++eye)
+  {
+    ovrSizei idealTextureSize = ovr_GetFovTextureSize(session, ovrEyeType(eye), hmd_desc.DefaultEyeFov[eye], 1);
+    /*eyeRenderTexture[eye] = new TextureBuffer(session, true, true, idealTextureSize, 1, NULL, 1);
+    eyeDepthBuffer[eye] = new DepthBuffer(eyeRenderTexture[eye]->GetSize(), 0);
+
+    if (!eyeRenderTexture[eye]->TextureChain)
+    {
+      if (retryCreate) goto Done;
+      VALIDATE(false, "Failed to create texture.");
+    }*/
+  }
+
+  ovrMirrorTexture mirror_texture = nullptr;
+  ovrMirrorTextureDesc mtd;
+  memset(&mtd, 0, sizeof(mtd));
+  mtd.Width = window_size.w;
+  mtd.Height = window_size.h;
+  mtd.Format = OVR_FORMAT_R8G8B8A8_UNORM_SRGB;
+
+  // Create mirror texture and an FBO used to copy mirror texture to back buffer
+  result = ovr_CreateMirrorTextureGL(session, &mtd, &mirror_texture);
+
 }
 
 void VRContext::deinit()
--- vr/test.cpp
+++ vr/test.cpp
@@ -11,11 +11,32 @@
 using namespace std;
 using namespace VR;
 
+class VRGame : public SDLGame
+{
+private:
+  void game_loop(const double game_time, const double frame_time) {}
+  void user_init() {}
+  void user_run() {}
+  void user_process_event(const SDL_Event &event) {}
+  void render_gl()
+  {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  }
+public:
+  VRGame() : SDLGame(640, 480, "VR Test") {}
+  ~VRGame() {}
+};
+
 int main(int argc, char **argv)
 {
-
   VRContext vr_context;
   vr_context.init();
+
+  VRGame game;
+  game.init();
+  
+  game.run();
 
   vr_context.deinit();
 
